8040dff3e3ced4b87e0e9cca215fedb786e48cf4
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-12-11 22:12:25 +0330

    import router and install

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import Home from './pages/Home/Home'
+import React from "react"
+import { Routes, Route, Router, BrowserRouter } from "react-router-dom"
+
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+import SignUp from "./pages/SignUp/SignUp"
+
+// const routes = (
+//   <Router>
+//     <Routes>
+//       <Route path="/dashboard"  element={<Home />} />
+//       <Route path="/login"  element={<Login />} />
+//       <Route path="/signup"  element={<SignUp />} />
+//     </Routes>
+//   </Router>
+// )
 
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1 className="text-red-300">Home</h1>
+      <h1 className="text-red-900">Home Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sint obcaecati impedit cum incidunt reiciendis temporibus delectus eligendi debitis ducimus.</h1>
     </div>
   )
 }
