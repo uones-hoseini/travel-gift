200adc510b5b8b80880b88a836d7b38643443f3c
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-12-13 14:01:48 +0330

    add profileinfo to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ProfileInfo from './Cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom'
 
+
+const navigate=useNavigate
 const Navbar = () => {
+
+const onLogout=()=>{
+  navigate("login")
+}
+
   return (
     <div className='bg-white flex items-center justify-between px-6 drop-shadow'>
       <h2 className="text-xl font-medium text-black py-2">Nots</h2>
-    <ProfileInfo/>
+    <ProfileInfo onLogout={onLogout}/>
 
     </div>
   )
