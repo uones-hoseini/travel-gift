f05b14dae03dfb2a8c995e608f4c2d006b508cb7
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-12-14 21:22:12 +0330

    Add card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ProfileInfo from '../../components/Cards/ProfileInfo'
-import Navbar from '../../components/Navbar'
+import React from "react"
+import ProfileInfo from "../../components/Cards/ProfileInfo"
+import Navbar from "../../components/Navbar"
+import NoteCard from "../../components/Cards/NoteCard"
 
 const Home = () => {
   return (
     <>
-    <Navbar/>
-    <div className="container mx-auto">
-      <NoteCard/>
-    </div>
+      <Navbar />
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+        <NoteCard title="Meeting on 7th April" date="3rd Apr 2024" content="Meeting on 7th April 3rd Apr 2024" tags="#Meeting" isPinned={true} onEdit={() => {}} onDelete={() => {}} onPinNote={() => {}} />
+       
+        </div>
+      </div>
     </>
   )
 }
