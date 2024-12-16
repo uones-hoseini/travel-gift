3aa0db7ae7c821be812f480fc4049c94686308b8
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-12-16 13:10:51 +0330

    Add TagInput component

--- src/pages/Home/AddEditeNotes.jsx
+++ src/pages/Home/AddEditeNotes.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import { useState } from "react"
+import TagInput from "../../components/input/TagInput"
+import { MdClose } from "react-icons/md"
 
-const AddEditeNotes = () => {
+const AddEditeNotes = ({noteData,type ,onClose }) => {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [tags, setTags] = useState([])
+
   return (
-    <div>
+    <div className="relative">
+      <div className="flex justify-end">
+        <button onClick={onClose} className="w-10 h-10 rounded-full flex items-center justify-center -top-3 -right-3 hover:bg-slate-50 ">
+          <MdClose className="text-xl text-slate-400" />
+        </button>
+      </div>
+
       <div className="flex flex-col gap-2">
-        <label className='input-lable' htmlFor="">TITLE</label>
-        <input className='text-2xl text-slate-950 outline-none' type="text"placeholder='Go To Gym At 5' />
+        <label className="input-lable">TITLE</label>
+        <input value={title} onChange={({ target }) => setTitle(target.value)} className="text-2xl text-slate-950 outline-none" type="text" placeholder="Go To Gym At 5" />
       </div>
       <div className="flex flex-col gap-2 mt-4">
-        <label className='input-lable' htmlFor="">CONTENT</label>
-        <textarea type='text'
-        className='text-sm to-slate-950 outline-none bg-slate-50 p-2 rounded'
-        placeholder='Content'
-        rows={10}
-        ></textarea>
+        <label className="input-lable">CONTENT</label>
+        <textarea value={content} onChange={({ target }) => setContent(target.value)} type="text" className="text-sm to-slate-950 outline-none bg-slate-50 p-2 rounded" placeholder="Content" rows={10}></textarea>
       </div>
       <div className="mt-3">
         <lable className="input-lable">TAGS</lable>
       </div>
-      <button 
-      onClick={()=>{}}
-      className='btn-primary font-medium mt-5 p-3'
-      >
+      <TagInput tags={tags} setTags={setTags} />
+      <button onClick={() => {}} className="btn-primary font-medium mt-5 p-3">
         ADD
-        </button>
+      </button>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
 import ProfileInfo from "../../components/Cards/ProfileInfo"
 import Navbar from "../../components/Navbar"
 import NoteCard from "../../components/Cards/NoteCard"
 import { MdAdd } from "react-icons/md"
 import AddEditeNotes from "./AddEditeNotes"
+import Modal from "react-modal"
 
 const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  })
   return (
     <>
       <Navbar />
       <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-4 mt-8">
-        <NoteCard title="Meeting on 7th April" date="3rd Apr 2024" content="Meeting on 7th April 3rd Apr 2024" tags="#Meeting" isPinned={true} onEdit={() => {}} onDelete={() => {}} onPinNote={() => {}} />
+          <NoteCard title="Meeting on 7th April" date="3rd Apr 2024" content="Meeting on 7th April 3rd Apr 2024" tags="#Meeting" isPinned={true} onEdit={() => {}} onDelete={() => {}} onPinNote={() => {}} />
         </div>
       </div>
 
-      <button onClick={()=>{}} className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary
-      hover:bg-blue-600 absolute right-10 bottom-10">
-        <MdAdd className=" text-[32px] text-white "/>
-      </button> 
-      <AddEditeNotes/>
-
+      <button
+        onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null })
+        }}
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary
+      hover:bg-blue-600 absolute right-10 bottom-10"
+      >
+        <MdAdd className=" text-[32px] text-white " />
+      </button>
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        ariaHideApp={false}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabel=" "
+        className="p-5 w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 overflow-scroll"
+      >
+        <AddEditeNotes onClose={()=>{setOpenAddEditModal({isShown:false,type:"add", data:null})}}/>
+      </Modal>
     </>
   )
 }
